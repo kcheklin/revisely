c483e616ba1c1e81b0d09341332e92e535ce3182
xinying0720 <lxinying653@gmail.com>
2025-11-30 16:59:25 +0800

    Frontend part1

---
+++ src/main/java/com/example/myapp/UpcomingSessionsFragment.java
@@ -0,0 +1,12 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UpcomingSessionsFragment extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Your friend will implement this
+    }
+}
